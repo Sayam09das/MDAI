8e329d43be13d8236723b3290dab23706b39bacb
Sayam09das <dassayam2021@gmail.com>
2025-12-21 23:25:37 +0530

    `Updated App component to display 'APP' instead of 'Pyana Dasgupta'`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 const App = () => {
   return (
-    <div className='bg-red-500 text-4xl'>Pyana Dasgupta</div>
+    <div className='bg-red-500 text-4xl'>APP</div>
   )
 }
 
