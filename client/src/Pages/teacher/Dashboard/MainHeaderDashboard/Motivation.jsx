1730cedd5c5798d847739019466b5aad7160addb
Sayam09das <dassayam2021@gmail.com>
2026-01-02 14:22:33 +0530

    `Removed unused files and components from teacher dashboard`

---
+++ client/src/Pages/teacher/Dashboard/MainHeaderDashboard/Motivation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Motivation = () => {
+  return (
+    <div>
+      Motivation
+    </div>
+  )
+}
+
+export default Motivation
--- client/src/Pages/teacher/Dashboard/MainHeaderDashboard/ReturnDashboard.jsx
+++ client/src/Pages/teacher/Dashboard/MainHeaderDashboard/ReturnDashboard.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import TeacherStats from './TeacherStats'
-import CourseOverview from './CourseOverview'
-import RecentActivity from './RecentActivity'
-import LiveClassSchedule from './LiveClassSchedule'
-import EarningsOverview from './EarningsOverview'
-import EngagementChart from './EngagementChart'
-import ProfileStatus from './ProfileStatus'
+import Motivation from './Motivation'
 
 const ReturnDashboard = () => {
     return (
         <div>
-            <ProfileStatus />
-            <TeacherStats />
-            <CourseOverview />
-            <RecentActivity />
-            <LiveClassSchedule />
-            <EarningsOverview />
-            <EngagementChart />
+            <Motivation />
         </div>
     )
 }
