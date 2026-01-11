8d198467522aaff408420bad5d2ae489b23aeb6f
Sayam09das <dassayam2021@gmail.com>
2026-01-11 16:28:39 +0530

    `Added ProtectedRoute component and updated App component to use it`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,36 @@
-import React from "react"
-import "./App.css"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import MainRoutes from "./routes/MainRoutes"
-import CourseRoutes from "./routes/CourseRoutes"
-import AuthRoutes from "./routes/AuthRoutes"
-import AboutRoutes from "./routes/AboutRoutes"
-import ContactRoutes from "./routes/ContactRoutes"
-import TeacherRoutes from "./routes/TeacherRoutes"
-import StudentRoutes from "./routes/StudentRoutes"
+import React from "react";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import MainRoutes from "./routes/MainRoutes";
+import CourseRoutes from "./routes/CourseRoutes";
+import AuthRoutes from "./routes/AuthRoutes";
+import AboutRoutes from "./routes/AboutRoutes";
+import ContactRoutes from "./routes/ContactRoutes";
+import TeacherRoutes from "./routes/TeacherRoutes";
+import StudentRoutes from "./routes/StudentRoutes";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 const App = () => {
   return (
     <>
       <ToastContainer />
+
+      {/* Public routes */}
       <MainRoutes />
       <CourseRoutes />
       <AboutRoutes />
       <ContactRoutes />
       <AuthRoutes />
-      <TeacherRoutes />
-      <StudentRoutes />
-    </>
-  )
-}
 
-export default App
+      {/* 🔐 Protected routes */}
+      <ProtectedRoute>
+        <TeacherRoutes />
+        <StudentRoutes />
+      </ProtectedRoute>
+    </>
+  );
+};
+
+export default App;
