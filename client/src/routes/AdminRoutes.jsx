f268128d257666279f283c6e1763a14ace522fa0
Sayam09das <dassayam2021@gmail.com>
2026-01-16 00:38:29 +0530

    `Renamed import and updated Route path in AdminRoutes component`

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import AdminRoutes from '../Admin/AdminRoutes'
-
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import AdminLayout from "../Admin/AdminRoutes" // 👈 renamed import
 
 const AdminRoutes = () => {
   return (
     <Routes>
-        <Route path="/admin" element={<AdminRoutes />} />
+      <Route path="/admin" element={<AdminLayout />} />
     </Routes>
   )
 }
 
-export default AdminRoutes;
+export default AdminRoutes
