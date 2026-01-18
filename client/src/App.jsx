1fb5eb7b96210e127320cf89d7ecd3dd6b4efda9
Sayam09das <dassayam2021@gmail.com>
2026-01-18 11:03:52 +0530

    `Update App.jsx to use React Router v6`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,40 @@
-import React from "react"
-import "./App.css"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import MainRoutes from "./routes/MainRoutes"
-import CourseRoutes from "./routes/CourseRoutes"
-import AuthRoutes from "./routes/AuthRoutes"
-import AboutRoutes from "./routes/AboutRoutes"
-import ContactRoutes from "./routes/ContactRoutes"
-import TeacherRoutes from "./routes/TeacherRoutes"
-import StudentRoutes from "./routes/StudentRoutes"
-import PaymentRoutes from "./routes/PaymentRoutes"
-import AdminRoutes from "./routes/AdminRoutes"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import MainRoutes from "./routes/MainRoutes";
+import CourseRoutes from "./routes/CourseRoutes";
+import AuthRoutes from "./routes/AuthRoutes";
+import AboutRoutes from "./routes/AboutRoutes";
+import ContactRoutes from "./routes/ContactRoutes";
+import TeacherRoutes from "./routes/TeacherRoutes";
+import StudentRoutes from "./routes/StudentRoutes";
+import PaymentRoutes from "./routes/PaymentRoutes";
+import AdminRoutes from "./routes/AdminRoutes";
+import NotFound from "./components/common/NotFound";
 
 const App = () => {
   return (
     <>
       <ToastContainer />
-      <MainRoutes />
-      <CourseRoutes />
-      <AboutRoutes />
-      <ContactRoutes />
-      <AuthRoutes />
-      <TeacherRoutes />
-      <StudentRoutes />
-      <PaymentRoutes />
-      <AdminRoutes />
-    </>
-  )
-}
 
-export default App
+      <Routes>
+        {/* App route groups */}
+        <Route path="/*" element={<MainRoutes />} />
+        <Route path="/*" element={<CourseRoutes />} />
+        <Route path="/*" element={<AboutRoutes />} />
+        <Route path="/*" element={<ContactRoutes />} />
+        <Route path="/*" element={<AuthRoutes />} />
+        <Route path="/*" element={<TeacherRoutes />} />
+        <Route path="/*" element={<StudentRoutes />} />
+        <Route path="/*" element={<PaymentRoutes />} />
+        <Route path="/*" element={<AdminRoutes />} />
+
+        {/* 🔥 GLOBAL 404 (LAST ALWAYS) */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
