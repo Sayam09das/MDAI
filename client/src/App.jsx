d942cfe1a1e600b1f754695ae931caf4b96792be
Sayam09das <dassayam2021@gmail.com>
2026-01-18 11:07:04 +0530

    `Removed unused routes and simplified App component`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,32 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-
-import MainRoutes from "./routes/MainRoutes";
-import CourseRoutes from "./routes/CourseRoutes";
-import AboutRoutes from "./routes/AboutRoutes";
-import ContactRoutes from "./routes/ContactRoutes";
-import AuthRoutes from "./routes/AuthRoutes";
-import TeacherRoutes from "./routes/TeacherRoutes";
-import StudentRoutes from "./routes/StudentRoutes";
-import PaymentRoutes from "./routes/PaymentRoutes";
-import AdminRoutes from "./routes/AdminRoutes";
-import NotFound from "./components/common/NotFound";
+import React from "react"
+import "./App.css"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import MainRoutes from "./routes/MainRoutes"
+import CourseRoutes from "./routes/CourseRoutes"
+import AuthRoutes from "./routes/AuthRoutes"
+import AboutRoutes from "./routes/AboutRoutes"
+import ContactRoutes from "./routes/ContactRoutes"
+import TeacherRoutes from "./routes/TeacherRoutes"
+import StudentRoutes from "./routes/StudentRoutes"
+import PaymentRoutes from "./routes/PaymentRoutes"
+import AdminRoutes from "./routes/AdminRoutes"
 
 const App = () => {
   return (
     <>
       <ToastContainer />
-
-      <Routes>
-        <Route path="/*" element={<MainRoutes />} />
-        <Route path="/*" element={<CourseRoutes />} />
-        <Route path="/*" element={<AboutRoutes />} />
-        <Route path="/*" element={<ContactRoutes />} />
-        <Route path="/*" element={<AuthRoutes />} />
-        <Route path="/*" element={<TeacherRoutes />} />
-        <Route path="/*" element={<StudentRoutes />} />
-        <Route path="/*" element={<PaymentRoutes />} />
-        <Route path="/*" element={<AdminRoutes />} />
-
-        {/* 🔥 MUST BE LAST */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <MainRoutes />
+      <CourseRoutes />
+      <AboutRoutes />
+      <ContactRoutes />
+      <AuthRoutes />
+      <TeacherRoutes />
+      <StudentRoutes />
+      <PaymentRoutes />
+      <AdminRoutes />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
