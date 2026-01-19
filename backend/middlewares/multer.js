5d25469974d0fb3a2c18191acdb4945abb30d747
Sayam09das <dassayam2021@gmail.com>
2026-01-19 20:08:47 +0530

    `Removed Cloudinary storage and switched to memory storage for multer`

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,18 +1,10 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "resources/thumbnails",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
 
 export default upload;
