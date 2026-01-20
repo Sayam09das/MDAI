8f556bd04adde3a549ff8b7d0cd4edea3f0af38c
Sayam09das <dassayam2021@gmail.com>
2026-01-20 20:23:23 +0530

    `Removed unused import from enrollment.routes.js`

--- backend/routes/enrollment.routes.js
+++ backend/routes/enrollment.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { enrollCourse, getMyEnrollments,getCourseEnrollmentsForTeacher } from "../controllers/enrollment.controller.js";
+import { enrollCourse, getMyEnrollments } from "../controllers/enrollment.controller.js";
 import { protect, userOnly } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
