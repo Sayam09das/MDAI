b056c93b52c6765e4329131394462a370c276270
Sayam09das <dassayam2021@gmail.com>
2026-01-21 19:01:43 +0530

    `Removed getCourseEnrollmentsForTeacher function and related routes`

--- backend/routes/enrollment.routes.js
+++ backend/routes/enrollment.routes.js
@@ -1,25 +1,10 @@
 import express from "express";
-import {
-    enrollCourse,
-    getMyEnrollments,
-    getCourseEnrollmentsForTeacher,
-} from "../controllers/enrollment.controller.js";
-
-import {
-    protect,
-    userOnly,
-    teacherOnly,
-} from "../middlewares/auth.middleware.js";
+import { enrollCourse, getMyEnrollments } from "../controllers/enrollment.controller.js";
+import { protect, userOnly } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/my-courses", protect, userOnly, getMyEnrollments);
-router.get(
-    "/teacher/course/:courseId",
-    protect,
-    teacherOnly,
-    getCourseEnrollmentsForTeacher
-);
 router.post("/:courseId", protect, userOnly, enrollCourse);
+router.get("/my-courses", protect, userOnly, getMyEnrollments);
 
 export default router;
