57b85bcb4542de7f86e87f6aacdb6f7f25a864a8
Sayam09das <dassayam2021@gmail.com>
2026-01-24 11:08:05 +0530

    add activity

--- admin/src/Dashboard/DashboardMain/ReturnDashboard.jsx
+++ admin/src/Dashboard/DashboardMain/ReturnDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DashboardOverview from './DashboardOverview'
+import ActivityOverview from './ActivityOverview'
 
 const ReturnDashboard = () => {
   return (
     <div>
         <DashboardOverview />
+        <ActivityOverview />
     </div>
   )
 }
