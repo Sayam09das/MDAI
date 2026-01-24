50563597755aa0ca751ed9284e505cafa6cdb468
Sayam09das <dassayam2021@gmail.com>
2026-01-24 11:24:20 +0530

    add files

--- admin/src/Dashboard/DashboardMain/ReturnDashboard.jsx
+++ admin/src/Dashboard/DashboardMain/ReturnDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import DashboardOverview from './DashboardOverview'
 import ActivityOverview from './ActivityOverview'
+import RecentActivity from './RecentActivity'
 
 const ReturnDashboard = () => {
   return (
     <div>
         <DashboardOverview />
         <ActivityOverview />
+        <RecentActivity />
     </div>
   )
 }
