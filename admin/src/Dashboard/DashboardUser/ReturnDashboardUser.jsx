0bf5c4f2f60b71da89971c9af9317529b2ec61ff
Sayam09das <dassayam2021@gmail.com>
2026-01-24 20:41:21 +0530

    add the student overview

--- admin/src/Dashboard/DashboardUser/ReturnDashboardUser.jsx
+++ admin/src/Dashboard/DashboardUser/ReturnDashboardUser.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StudentAnalytics from './StudentAnalytics'
+import StudentMetricsOverview from './StudentMetricsOverview'
 
 const ReturnDashboardUser = () => {
   return (
     <div>
         <StudentAnalytics />
+        <StudentMetricsOverview />
     </div>
   )
 }
