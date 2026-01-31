64253e678e59b761a15fc8c113f23266119b6e7a
Sayam09das <dassayam2021@gmail.com>
2026-01-31 17:46:11 +0530

    `Removed route parameter from receipt URL`

--- backend/routes/receipt.routes.js
+++ backend/routes/receipt.routes.js
@@ -3,6 +3,6 @@ import { protect } from "../middlewares/auth.middleware.js";
 import { getSignedReceiptUrl } from "../controllers/receipt.controller.js";
 const router = express.Router();
 
-router.get("/receipt/:publicId", protect, getSignedReceiptUrl);
+router.get("/receipt", protect, getSignedReceiptUrl);
 
 export default router;
